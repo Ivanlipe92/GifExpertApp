bb7764811d41da2d2a754a8aafe5465b14aa0f9a
Ivanlipe92 <ivanlipe92@hotmail.com>
2025-01-30 23:00:10 -0500

    agregue un lorem

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -18,7 +18,7 @@ export const GifGrid = ({category}) => {
     return (
         <>
             <h3>{category}</h3>
-            
+            <h2>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias similique veniam quis itaque ducimus dicta obcaecati asperiores. Ea eligendi blanditiis ex qui beatae quam deserunt, voluptate inventore error fugit maiores.</h2>
         </>
     )
 }
